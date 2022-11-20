54bf9f32787f9fe53864d7a3ff77246e14bd77b1
Saket Verma <saket3324verma@gmail.com>
2022-11-20 19:17:08 +0530

    Deletion complete

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import * as e from 'cors';
+import { DataAndroidService } from '../data-android.service';
+import { DataIosService } from '../data-ios.service';
 import { DataService } from '../data.service';
 
 @Component({
@@ -13,30 +16,38 @@ export class SidebarComponent implements OnInit {
 
   public isOpen: boolean = true;
   public appName: any = {};
+  public isDeleting: boolean = false;
   screen: any;
 
-  constructor(public data: DataService, private router:Router) { }
+  constructor(
+    public data: DataService, 
+    private router: Router, 
+    private ios: DataIosService, 
+    private android: DataAndroidService) {}
 
   ngOnInit(): void {
     this.screen = screen;
-    this.data.shouldUpdate.subscribe(shouldUpdate => {
+    this.data.shouldUpdate.subscribe((shouldUpdate) => {
       if (shouldUpdate) {
         setTimeout(() => {
           let app = this.data.getSelectedApp();
+          this.apps.forEach((el) => {
+            el.isSelectedForDelete = false;
+          });
           this.appName = app;
-          if (this.apps.find(x => x.altName == app.altName) != undefined) {
-            this.apps.find(x => x.altName == app.altName).isSelected = true;
+          if (this.apps.find((x) => x.altName == app.altName) != undefined) {
+            this.apps.find((x) => x.altName == app.altName).isSelected = true;
           }
-        })
+        });
       }
     });
 
-    this.data.sideBarBehavior.subscribe(data=> {
-      if(data.bool) {
+    this.data.sideBarBehavior.subscribe((data) => {
+      if (data.bool) {
         this.toggleSideBar();
-        this.router.navigate(["/reviews"]);
+        this.router.navigate(['/reviews']);
       }
-    })
+    });
   }
 
   selectApp(appSelected: any) {
@@ -44,15 +55,63 @@ export class SidebarComponent implements OnInit {
       app.isSelected = false;
     });
     appSelected.isSelected = true;
-    this.toggleSideBar();
-    this.router.navigate(["/reviews"]);
+    if (this.screen.width < 768) {
+      this.toggleSideBar();
+    }
+    this.router.navigate(['/reviews']);
     this.selectedApp.emit(appSelected);
   }
 
   toggleSideBar() {
-    if(screen.width < 768) {
-      this.isOpen = !this.isOpen;
-      this.data.setSide(this.isOpen);
+    // if(screen.width < 768) {
+    this.isOpen = !this.isOpen;
+    this.data.setSide(this.isOpen);
+    // }
+  }
+
+  toggleDelete() {
+    this.isDeleting = !this.isDeleting;
+    if (this.isDeleting) {
+      this.apps.forEach((app) => {
+        app.isSelected = false;
+        app.isSelectedForDelete = false;
+      });
     }
+  }
+
+  selectForDelete(app: any) {
+    this.apps.find((x) => x.altName == app.altName).isSelectedForDelete =
+      !this.apps.find((x) => x.altName == app.altName).isSelectedForDelete;
+  }
+
+  deleteApps() {
+    let ios: any[] =[];
+    let android: any[] =[];
+    this.apps.forEach((app) => {
+      if(app.isSelectedForDelete) {
+        if(app.isIOS) {
+          ios.push(app.id);
+        } else {
+          android.push(app.appId)
+        }
+      }
+    });
+    console.log({ios: ios, android: android});
+    if(ios.length > 0) {
+      let values = this.ios.getIOSApps();
+      ios.forEach(el=> {
+        values.splice(values.findIndex(appToFind => appToFind.id === el), 1);
+      })
+      this.ios.setIOSApps(values);
+    }
+    if(android.length > 0) {
+      let values = this.android.getAndroidApps();
+      android.forEach(el=> {
+        values.splice(values.findIndex(appToFind => appToFind.appId === el), 1);
+      })
+      this.android.setAndroidApps(values);
+    }
+    this.data.shouldUpdate.next(true);
+    this.toggleDelete();
   }
 }
